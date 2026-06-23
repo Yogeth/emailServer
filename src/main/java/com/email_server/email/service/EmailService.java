4c765654e3a3bc29ff5ae73482a4b3b7dcac0184
yogeth <yogethcr@gmail.com>
2026-06-23 15:15:52 +0530

    first commit

---
+++ src/main/java/com/email_server/email/service/EmailService.java
@@ -0,0 +1,31 @@
+package com.email_server.email.service;
+
+import com.email_server.email.entity.EmailEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    private final String sender = "yogethcr@gmail.com";
+
+    public String sendMail(EmailEntity emailEntity)
+    {
+       try{
+           SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+           mailMessage.setFrom(sender);
+           mailMessage.setTo(emailEntity.getReciver());
+           mailMessage.setText(emailEntity.getMessgeBody());
+           javaMailSender.send(mailMessage);
+
+           return "Mail Sent Sucessfully!";
+       }catch(Exception e){
+           return "Error while Sending Mail !";
+       }
+    }
+}
